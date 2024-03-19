3aae3ea79edae986c66ca16fe73fcc4be3e81a2c
zahidjaved <zahidjaved121@gmail.com>
2024-03-19 13:55:06 -0700

    portfolior

---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+// init Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    getSortData: {
+      name: '.name',
+      symbol: '.symbol',
+      number: '.number parseInt',
+      category: '[data-category]',
+      weight: function( itemElem ) {
+        var weight = $( itemElem ).find('.weight').text();
+        return parseFloat( weight.replace( /[\(\)]/g, '') );
+      }
+    }
+
+  });
+  
+
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+  
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+    
+
+  // swiper
+  var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+      grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  
+  // set counter
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#number1').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $.fn.jQuerySimpleCounter = function (options) {
+      var settings = $.extend({
+          start: 0,
+          end: 100,
+          easing: "swing",
+          duration: 400,
+          complete: "",
+        },
+        options
+      );
+
+      var thisElement = $(this);
+
+      $({
+        count: settings.start,
+      }).animate({
+        count: settings.end,
+      }, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function () {
+          var mathCount = Math.ceil(this.count);
+          thisElement.text(mathCount);
+        },
+        complete: settings.complete,
+      });
+    };
+
+    $("#number1").jQuerySimpleCounter({
+      end: 100,
+      duration: 3000,
+    });
+    $("#number2").jQuerySimpleCounter({
+      end: 98,
+      duration: 3000,
+    });
+    $("#number3").jQuerySimpleCounter({
+      end: 10,
+      duration: 2000,
+    });
+    counted = 1;
+  }
+
+});
+
